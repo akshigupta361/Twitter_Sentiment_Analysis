cf7f8130af3863a6e26e69da41b09b6f9d2a20be
akshigupta361 <guptaakshi361@gmail.com>
2020-07-20 00:13:07 +0530

    Analyzing Dataset

--- index.py
+++ index.py
@@ -20,3 +20,12 @@ from sklearn.metrics import confusion_matrix, classification_report,accuracy_sco
 train_tweets = pd.read_csv('twitter_train.csv')
 test_tweets = pd.read_csv('twitter_test.csv')
 
+#Data Analysis
+sns.countplot(x= 'label',data = train_tweets)
+
+def form_sentence(tweet):
+    tweet_blob = TextBlob(tweet)
+    return ' '.join(tweet_blob.words)
+
+print(form_sentence(train_tweets['tweet'].iloc[10]))
+print(train_tweets['tweet'].iloc[10])
